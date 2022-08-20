d47575d4f398112c40d8e4e87872c7a8d06e047c
jay <10854841444@qq.com>
2022-08-20 11:45:23 +0800

    删除接口中的函数的public前缀

--- src/cn/jay/ch4_graph/a0_base/Graph.java
+++ src/cn/jay/ch4_graph/a0_base/Graph.java
@@ -1,13 +1,11 @@
 package cn.jay.ch4_graph.a0_base;
 
 public interface Graph {
-    public int getV();
+    int getV();
 
-    public int getE();
+    int getE();
 
-    public void addEdge(int v1, int v2);
+    void addEdge(int v1, int v2);
 
     Iterable<Integer> adj(int v);
-
-    String toString();
 }

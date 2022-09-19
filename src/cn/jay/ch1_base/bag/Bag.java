e1547bfa85e2b960a78d8c6011d73b628bec8431
jay <1085484144@qq.com>
2022-09-20 00:40:35 +0800

    docs: 添加注释

--- src/cn/jay/ch1_base/bag/Bag.java
+++ src/cn/jay/ch1_base/bag/Bag.java
@@ -1,8 +1,26 @@
 package cn.jay.ch1_base.bag;
 
 
-public interface Bag<T> extends Iterable<T>{
+public interface Bag<T> extends Iterable<T> {
+
+    /**
+     * 向 bag 中添加元素
+     * @param t 待添加的元素
+     */
     void add(T t);
+
+    /**
+     * 返回当前 bag 中的元素个数
+     * @return int 元素个数
+     */
     int size();
-    boolean isEmpty();
+
+    /**
+     * 判断当前 bag 是否为空
+     * 使用 {@link #size()}实现
+     * @return 为空返回 true, 否则返回 false
+     */
+    default boolean isEmpty() {
+        return size() == 0;
+    }
 }

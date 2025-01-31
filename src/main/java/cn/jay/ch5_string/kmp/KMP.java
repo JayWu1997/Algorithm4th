a7e6056f51b930cb3e8468e218b108a4e63fd8bf
jay <1085484144@qq.com>
2025-01-31 14:59:20 +0800

    refactor(KMP):

--- src/main/java/cn/jay/ch5_string/kmp/KMP.java
+++ src/main/java/cn/jay/ch5_string/kmp/KMP.java
@@ -1,71 +1,84 @@
 package cn.jay.ch5_string.kmp;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
+ * KMP算法的实现，需要先了解<a href="https://oi-wiki.org/string/automaton/">自动机</a>
+ * <p>
+ * eg: 对于 pattern = "abcabc"<p>
+ * 正确状态列表: state(0){}, state(1){a}, state(2){ab}, state(3){abc}, state(4){abca}, state(5){abcab}, state(6){abcabc}, 且 state(6)为最终状态<p>
+ * 每个状态的最长相等前后缀长度: next[0] = 0, next[1] = 0, next[2] = 0, next[3] = 0, next[4] = 1, next[5] = 2, next[6] = 3 <p>
+ * 如何计算每个状态对应的最长相等前后缀呢? next[i] = stateTransitionEquation(next[i-1], pattern.charAt(i-1));
  * @author jay
- * @date 2022/12/03 12:51:08
- * @desc KMP算法的实现，关键在 next 数组的计算，指定位置不匹配时应该回退到哪个位置再进行比较，这里只需找到和后缀相同的前缀
- * 最重要的是 next数组的计算，next数组表示前缀与后缀最长相同子串的长度，表示当 pattern第 index 位字符不能匹配指定String时，跳转到 next[index]处重新比较
- * 比如 pattern 为 abcabdef，其 next 数组为 [0, 0, 0, 1, 2, 0, 0, 0]
  */
 public class KMP {
 
+    private String pattern;
+
     /**
-     * 计算 next 数组
-     *
-     * @param pat 即 pattern
-     * @return 计算好的 next数组
+     * 当前传入的 c 与 "转换到下一个正确状态所需的字符" 不匹配时, 应该跳转到哪个状态继续比较
      */
-    private int[] calNext(String pat) {
-        int[] next = new int[pat.length()];
+    private int[] next;
+
+    public KMP(String pattern) {
+        this.pattern = pattern;
+        next = new int[pattern.length() + 1];
         next[0] = 0;
-
-        // 动态规划的思想，j表示当前要比较的字符的下标
-        for (int i = 1, j = 0; i < pat.length(); i++){
-            while(j > 0 && pat.charAt(j) != pat.charAt(i)){
-                j = next[j-1];
-            }
-
-            if(pat.charAt(j) == pat.charAt(i)){
-                j++;
-            }
-
-            next[i] = j;
-        }
-        return next;
+        computeMaxPrePostFixLength();
     }
 
     /**
-     * 返回所有匹配的起始位置
-     * @param pattern
-     * @param str
+     * <a href="https://oi-wiki.org/string/automaton/#kmp-%E8%87%AA%E5%8A%A8%E6%9C%BA">状态转移方程</a>
+     * </p>
+     * @param state 当前状态
+     * @param c 待对比的字符
      * @return
      */
-    private List<Integer> search(String pattern, String str){
-        List<Integer> list = new ArrayList<Integer>();
-        int[] next = calNext(pattern);
-        for (int i = 0, j = 0; i < str.length(); i++){
-            while(j > 0 && pattern.charAt(j) != str.charAt(i)){
-                j = next[j];
-            }
-            if(pattern.charAt(j) == str.charAt(i)){
-                j++;
-            }
-
-            if(j == pattern.length()){
-                list.add(i-j+1);
-                j = 0;
-            }
+    private int stateTransitionEquation(int state, char c) {
+        // 当前状态转向下一正确状态所需的字符pattern.charAt(state) 是否等于字符 c
+        if (pattern.charAt(state) == c) {
+            return state + 1;
+        } else if (state == 0) {
+            return 0;
+        } else {
+            return stateTransitionEquation(next[state], c);
         }
-        return list;
+    }
+
+    private void computeMaxPrePostFixLength() {
+        // 状态 0 和 1 的最长相等前后缀长度为 0, 因为其字符串长度小于 2
+        next[0] = 0;
+        next[1] = 0;
+        for (int i = 2; i <= pattern.length(); i++) {
+            next[i] = stateTransitionEquation(next[i-1], pattern.charAt(i-1));
+        }
+        System.out.println("sMPPFL: " + Arrays.toString(next));
+    }
+
+    public List<Integer> match(String text) {
+        List<Integer> result = new ArrayList<>();
+        int i = 0;
+        int j = 0;
+        while (i < text.length()) {
+            // 状态转移
+            j = stateTransitionEquation(j, text.charAt(i));
+            // 若 j = 结束状态
+            if (j == pattern.length()) {
+                result.add(i - pattern.length() + 1);
+                // 继续匹配
+                j = next[pattern.length()];
+            }
+            i++;
+        }
+        return result;
     }
 
     public static void main(String[] args) {
-        KMP kmp = new KMP();
-        System.out.println(kmp.search("ababc", "haabcababcabcdababcfjkal"));
-
+        String pattern = "ababcabcabab";
+        String text = "ababcabcabababcabcabababcabcabababcabcabab";
+        KMP kmp = new KMP(pattern);
+        System.out.println(kmp.match(text));
     }
-
 }
